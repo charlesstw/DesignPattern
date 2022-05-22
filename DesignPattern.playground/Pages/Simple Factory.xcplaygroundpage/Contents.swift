5b5770fbaa3ba4a0435a5a2678898754a753f20c
CharlesWang <charleswang@taiwanmobile.com>
2022-05-22 18:13:46 +0800

    @Purpose [update] 實作 Simple Factory

--- DesignPattern.playground/Pages/Simple Factory.xcplaygroundpage/Contents.swift
+++ DesignPattern.playground/Pages/Simple Factory.xcplaygroundpage/Contents.swift
@@ -1,7 +1,60 @@
-//: [Previous](@previous)
-
+/*: [Previous](@previous)
+ 簡單工廠 Simple Factory
+ 又稱靜態工廠，一般來說同工廠內產生的類別會有共同的父類別
+ 隨著輸入參數不同，簡單工廠會回傳不同的物件
+*/
 import Foundation
+import XCTest
 
-var str = "Hello, playground"
+// 冒險者
+protocol Adventurer {
+    func getType() -> AdventurerType
+}
+
+// 弓箭手
+class Archer: Adventurer {
+    func getType() -> AdventurerType {
+        return .archer
+    }
+}
+
+// 騎士 Knight
+class Knight: Adventurer {
+    func getType() -> AdventurerType {
+        return .knight
+    }
+}
+
+enum AdventurerType {
+    case archer
+    case knight
+}
+
+/*
+ 訓練營 (Simple Factory)
+ */
+class TrainingCamp {
+    class func trainAdventurer(type: AdventurerType) -> Adventurer {
+        switch type {
+        case .knight:
+            return Knight()
+        case .archer:
+            return Archer()
+        }
+    }
+}
+
+/*
+ 測試
+ */
+
+class SimpleFactoryTest: XCTestCase {
+    func testTrainAdventurer() {
+        let archer = TrainingCamp.trainAdventurer(type: .archer)
+        let knight = TrainingCamp.trainAdventurer(type: .knight)
+        XCTAssertEqual(archer.getType(), .archer)
+        XCTAssertEqual(knight.getType(), .knight)
+    }
+}
 
 //: [Next](@next)
